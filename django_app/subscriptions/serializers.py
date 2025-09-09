b1ed14e1a743eb99b2919e283ceecbcfd343d339
magdy <mmagdy841@gmail.com>
2025-09-10 00:29:49 +0300

    created serializers for subscriptions

---
+++ django_app/subscriptions/serializers.py
@@ -0,0 +1,85 @@
+# subscriptions/serializers.py
+from rest_framework import serializers
+from .models import SubscriptionPlan, Subscription, Payment
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+# ------------------------
+# SubscriptionPlan
+# ------------------------
+class SubscriptionPlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubscriptionPlan
+        fields = [
+            "id",
+            "name",
+            "price",
+            "duration_months",
+            "stripe_plan_id",
+            "created_at",
+        ]
+
+
+# ------------------------
+# Payment
+# ------------------------
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = [
+            "id",
+            "stripe_payment_id",
+            "amount",
+            "status",
+            "created_at",
+        ]
+
+
+# ------------------------
+# Subscription
+# ------------------------
+class SubscriptionListSerializer(serializers.ModelSerializer):
+    plan = SubscriptionPlanSerializer(read_only=True)
+
+    class Meta:
+        model = Subscription
+        fields = [
+            "id",
+            "plan",
+            "status",
+            "start_date",
+            "end_date",
+        ]
+
+
+class SubscriptionDetailSerializer(serializers.ModelSerializer):
+    plan = SubscriptionPlanSerializer(read_only=True)
+    payments = PaymentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Subscription
+        fields = [
+            "id",
+            "plan",
+            "status",
+            "start_date",
+            "end_date",
+            "payments",
+        ]
+
+
+# ------------------------
+# User-Scoped Subscription
+# ------------------------
+class UserSubscriptionSerializer(serializers.ModelSerializer):
+    subscriptions = SubscriptionListSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "subscriptions",
+        ]
